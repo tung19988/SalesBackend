12482d720ec993c698e20dafca777228257d5380
tung19988 <tungbuithanh2498@gmail.com>
2019-10-16 18:39:52 +0700

    sua department dao

--- src/main/java/com/dao/DepartmentDao.java
+++ src/main/java/com/dao/DepartmentDao.java
@@ -13,6 +13,6 @@ public interface DepartmentDao {
 	public List<Department> findAll() ;
 	public Optional<Department> findById(int id) ;
 	public void save(Department de) ;
-	public void delete(Department de) ;
+	public void deleteByID(int id) ;
 	
 }
